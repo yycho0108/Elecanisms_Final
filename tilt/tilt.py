ceaa6a48dec56c2597c7b29857d4e752b05a9009
yycho0108 <jchocholate@gmail.com>
2017-04-20 11:37:33 -0400

    attempts at permanent bonding, will look more into it later

--- tilt/tilt.py
+++ tilt/tilt.py
@@ -16,48 +16,48 @@ def read_ser(ser):
 
 def main():
     processor = EventProcessor()
-    board = Wiiboard(processor)
+    with Wiiboard(processor,address="34:AF:2C:2D:25:22") as board:
+        #print "Trying to connect..."
+        #board.connect("34:AF:2C:2D:25:22")  # The wii board must be in sync mode at this time
+        #board.connect()
+        board.wait(200)
 
-    print "Trying to connect..."
-    board.connect()  # The wii board must be in sync mode at this time
-    board.wait(200)
+        # Flash the LED so we know we can step on.
+        board.setLight(False)
+        board.wait(500)
+        board.setLight(True)
+        board.async_receive()
 
-    # Flash the LED so we know we can step on.
-    board.setLight(False)
-    board.wait(500)
-    board.setLight(True)
-    board.async_receive()
+        ## Communicate via UART - debugging
+        #with serial.Serial(
+        #        port = '/dev/ttyAMA0',
+        #        baudrate = 19200,
+        #        parity = serial.PARITY_NONE,
+        #        stopbits=serial.STOPBITS_ONE,
+        #        timeout=None) as ser:
 
-    ## Communicate via UART - debugging
-    #with serial.Serial(
-    #        port = '/dev/ttyAMA0',
-    #        baudrate = 19200,
-    #        parity = serial.PARITY_NONE,
-    #        stopbits=serial.STOPBITS_ONE,
-    #        timeout=None) as ser:
+        #    r_t = threading.Thread(target = read_ser, args=(ser,))
+        #    r_t.daemon = True
+        #    r_t.start()
 
-    #    r_t = threading.Thread(target = read_ser, args=(ser,))
-    #    r_t.daemon = True
-    #    r_t.start()
+        pic = PICInterface()
 
-    pic = PICInterface()
+        while True:
+            if pic.connected:
+                t_x, t_y = processor.t_x, processor.t_y # tilt angles
+                #t_x, t_y = 0,10 # for testing
+                t_x, t_y = t_y, t_x # this is flipped due to servo position
+                s_x, s_y = tilt2servo(t_x, rad=False), tilt2servo(t_y, rad=False) # servo angles
 
-    while True:
-        if pic.connected:
-            t_x, t_y = processor.t_x, processor.t_y # tilt angles
-            #t_x, t_y = 0,10 # for testing
-            t_x, t_y = t_y, t_x # this is flipped due to servo position
-            s_x, s_y = tilt2servo(t_x, rad=False), tilt2servo(t_y, rad=False) # servo angles
+                print 'writing tilt : ({0:.2f}, {1:.2f}); servo : ({2:.2f},{3:.2f})'.format(t_x, t_y, s_x, s_y)
+                if not (pic.write_x(s_x) and pic.write_y(s_y)):
+                    pic.connected = False
+            else:
+                pic.connect()
 
-            print 'writing tilt : ({0:.2f}, {1:.2f}); servo : ({2:.2f},{3:.2f})'.format(t_x, t_y, s_x, s_y)
-            if not (pic.write_x(s_x) and pic.write_y(s_y)):
-                pic.connected = False
-        else:
-            pic.connect()
+            time.sleep(0.05)
 
-        time.sleep(0.05)
-
-    pic.close()
+        pic.close()
 
 if __name__ == "__main__":
     main()
