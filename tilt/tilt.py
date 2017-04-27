8e988639f72f1de45cddcd06940fb921a67e21af
Jamie's RasPi <jchocholate@gmail.com>
2017-04-27 06:42:19 +0000

    sfx

--- tilt/tilt.py
+++ tilt/tilt.py
@@ -7,19 +7,27 @@ from get_angle import tilt2servo
 import serial
 import time
 import threading
+import pygame
 
-def read_ser(ser):
+pygame.mixer.init()
+pygame.mixer.music.load('sound/background.mp3')
+pygame.mixer.music.play(loops=-1, start=0.0)
+pygame.mixer.music.set_volume(1.0)
+
+e_sound = pygame.mixer.Sound("sound/electromagnet.wav")
+
+
+def read_cmd(ser):
     while ser.isOpen():
-        s = '>' + ser.readline()
-        print s 
+        cmd = ser.readline()
+        if 'electromagnet' in cmd:
+            e_sound.play()
         time.sleep(0.04)
 
 def main():
     processor = EventProcessor()
-    with Wiiboard(processor,address="34:AF:2C:2D:25:22") as board:
+    with Wiiboard(processor) as board:
         #print "Trying to connect..."
-        #board.connect("34:AF:2C:2D:25:22")  # The wii board must be in sync mode at this time
-        #board.connect()
         board.wait(200)
 
         # Flash the LED so we know we can step on.
@@ -29,16 +37,16 @@ def main():
         board.async_receive()
 
         ## Communicate via UART - debugging
-        #with serial.Serial(
-        #        port = '/dev/ttyAMA0',
-        #        baudrate = 19200,
-        #        parity = serial.PARITY_NONE,
-        #        stopbits=serial.STOPBITS_ONE,
-        #        timeout=None) as ser:
+        with serial.Serial(
+                port = '/dev/ttyUSB0',
+                baudrate = 19200,
+                parity = serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                timeout=None) as ser:
 
-        #    r_t = threading.Thread(target = read_ser, args=(ser,))
-        #    r_t.daemon = True
-        #    r_t.start()
+            r_t = threading.Thread(target = read_cmd, args=(ser,))
+            r_t.daemon = True
+            r_t.start()
 
         pic = PICInterface()
 
