47a4f4c003b538b259bfb8ebea634c7fead38173
yycho0108 <jchocholate@gmail.com>
2017-04-27 02:17:04 -0400

    working code -- now testing wii board

--- tilt/tilt.c
+++ tilt/tilt.c
@@ -18,60 +18,61 @@
 
 #define cap(mn,x,mx) ((mx)<(x))?(mx):((mn)>(x))?(mn):(x)
 
-#define X_SERVO_PIN &D[0]
-#define Y_SERVO_PIN &D[1]
+// ======== PIN SETUP =========
+#define W_X_SERVO_PIN &D[0]
+#define W_Y_SERVO_PIN &D[1]
 
-#define COIN_PIN &D[2]
+#define R_COIN_PIN &D[2]
 
 // servos for paddle
-#define L_SERVO_PIN &D[3]
-#define R_SERVO_PIN &D[4]
+#define W_L_SERVO_PIN &D[3]
+#define W_R_SERVO_PIN &D[4]
 
 // center rotary wall
-#define C_SERVO_PIN &D[5]
-
-//Ball Release
-#define BALL_RELEASE_SERVO &D[7]
+#define W_C_SERVO_PIN &D[5]
 
 // Electromagnet control Pins
-#define ELECTRO_PIN &D[6]
+#define W_ELECTRO_PIN &D[6]
 
-//Read if player 2 is ready
-#define START_PIN &D[10]
-
+//Ball Release
+#define W_B_SERVO_PIN &D[7]
 
 // LCD Pins
 #define LCD_SDA_PIN &D[8]
 #define LCD_SCL_PIN &D[9]
 
-// #define BALL_START_PIN &D[10]
-#define L_READ_PIN &D[11]
-#define R_READ_PIN &D[12]
-#define END_PIN &D[13]
+//Read if player 2 is ready
+#define R_START_PIN &D[10]
 
+// #define BALL_R_START_PIN &D[10]
+#define R_L_SERVO_PIN &D[11]
+#define R_R_SERVO_PIN &D[12]
+#define R_END_PIN &D[13]
 
-#define ELECTRO_READ_PIN &A[1]
+// center servo potentiometer
+#define R_C_SERVO_PIN &A[0]
+// read electromagnet, treat it "like" a digital pin
+#define R_ELECTRO_PIN &A[1]
 
-#define C_READ_PIN &A[0]
+// ======== PIN SETUP =========
 
 enum {IDLE, WAIT_COIN, SETUP_BOARD, WAIT_PLAYERS, RUN, END};
 
 #define true 1
 #define false 0
-
-#define COOLDOWN_PER 4
-#define ELECTRO_ON_PER 2
-
 typedef unsigned char bool;
-uint8_t state = 0;
-uint8_t electromag_counter=0;
-uint8_t electromag_counter_check = 0;
-volatile bool electromagnet_on = false;
-volatile bool electromag_pressed = false;
 
+
+uint8_t state = 0;
+
+/* ELECTROMAGNET */
+#define EM_COOLDOWN_PER 8 // cooldown 4 sec.
+#define EM_ON_PER 8 // on for 4 sec.
+enum {EM_READY, EM_ON, EM_COOLDOWN};
+volatile unsigned char electromagnet_state = EM_READY;
+uint8_t electromag_counter=0;
 
 // SERVO_OFFSET defines offset from "horizontal"
-
 // HS805BB Datasheet : https://www.servocity.com/hs-805bb-servo 
 // MAX TRAVEL : 199.5 deg.
 // PWM Range : 556-2420 us
@@ -141,6 +142,7 @@ void coin_inserted(){
 	//	return;
 	//}
 	if(!coin){
+		printf("sfx:coin\n");
 		printf("COIN INSERTED : %d\n", coin);
 		coin = true;
 		led_toggle(&led3);
@@ -165,59 +167,68 @@ void ball_ready(){
 //State machine helper functions
 void do_balance(void){
 	// TODO : account for offset
-	pin_write(X_SERVO_PIN, calc_servo_pos(0));
-	pin_write(Y_SERVO_PIN, calc_servo_pos(0));
+	pin_write(W_X_SERVO_PIN, calc_servo_pos(0));
+	pin_write(W_Y_SERVO_PIN, calc_servo_pos(0));
 }
 
 void center_ob(void){
-	float c_pot = pin_read(C_READ_PIN)/65535. - 0.5;
+	float c_pot = pin_read(R_C_SERVO_PIN)/65535. - 0.5;
 	c_pot *= 180;
-	pin_write(C_SERVO_PIN, calc_servo_pos(c_pot));
+	//printf("%f\n", c_pot);
+	pin_write(W_C_SERVO_PIN, calc_servo_pos(c_pot));
 }
 
 void flipper_ob(void){
-	pin_write(R_SERVO_PIN, calc_servo_pos(pin_read(L_READ_PIN)*90));
-	pin_write(L_SERVO_PIN, calc_servo_pos((1 - pin_read(R_READ_PIN))*90));
+	unsigned char f_l = pin_read(R_L_SERVO_PIN);
+	unsigned char f_r = pin_read(R_R_SERVO_PIN);
+
+	if(f_l || f_r){
+		printf("sfx:flipper\n");
+	}
+
+	pin_write(W_R_SERVO_PIN, calc_servo_pos(f_l*90));
+	pin_write(W_L_SERVO_PIN, calc_servo_pos((1 - f_r)*90));
 }
 
 void electromag_ob(void){
-	if(timer_flag(&timer4)){//wait for electromagnet to be supposed to turn off
-		//or for cooldown period to end
-		if (electromag_counter >= electromag_counter_check){
-			// Check if it's been enough seconds
-			if (electromagnet_on){
-				timer_lower(&timer4);
-				electromag_counter = 0;
-				//Turn off the electromagnet, then start cooldown
-				electromagnet_on = false;
-				electromag_counter_check = COOLDOWN_PER;
-				timer_start(&timer4);
+	unsigned char e = pin_read(R_ELECTRO_PIN);
+	bool em_pressed = pin_read(R_ELECTRO_PIN) > 32768;
 
-			}
-			else if(pin_read(ELECTRO_READ_PIN)>32768){
-				//Turn off electromagnet
-				timer_lower(&timer4);
-				electromag_counter_check = ELECTRO_ON_PER;
-				timer_start(&timer4);
-				electromagnet_on = true;
-				electromag_counter = 0;
-			}
-		}
-		else{
-			electromag_counter = electromag_counter+1;
-			timer_lower(&timer4);
-		}
-		
-		pin_write(ELECTRO_PIN,electromagnet_on);
-		led_write(&led1,electromagnet_on);
-		if(pin_read(ELECTRO_READ_PIN)>32768){
-			electromag_pressed = true;
-		}
-		else{
-			electromag_pressed = false;
-		}
-		led_write(&led2,electromag_pressed);
+	led_write(&led1,em_pressed);
+
+	if(timer_flag(&timer4)){
+		timer_lower(&timer4);
+		++electromag_counter;
 	}
+
+	switch(electromagnet_state){
+		case EM_READY:
+			if(em_pressed){
+				printf("sfx:electromagnet\n");
+				pin_write(W_ELECTRO_PIN, 1);
+				electromagnet_state = EM_ON;
+				timer_stop(&timer4);
+				timer_setPeriod(&timer4, 0.5);
+				timer_start(&timer4);
+				electromag_counter = 0; // reset counter
+			}
+			break;
+		case EM_ON:
+			if(electromag_counter > EM_ON_PER){
+				pin_write(W_ELECTRO_PIN, 0);
+				electromagnet_state = EM_COOLDOWN;
+				timer_stop(&timer4);
+				timer_start(&timer4);
+				electromag_counter = 0; // reset counter
+			}
+			break;
+		case EM_COOLDOWN:
+			if(electromag_counter > EM_COOLDOWN_PER){
+				electromagnet_state = EM_READY;
+			}
+			break;
+	}
+
 }
 
 void do_obstacles(void){
@@ -228,8 +239,8 @@ void do_obstacles(void){
 }
 
 void do_wii(void){
-	pin_write(X_SERVO_PIN, calc_servo_pos(s_x+10));
-	pin_write(Y_SERVO_PIN, calc_servo_pos(s_y+2)); //account for offset
+	pin_write(W_X_SERVO_PIN, calc_servo_pos(s_x+10));
+	pin_write(W_Y_SERVO_PIN, calc_servo_pos(s_y+2)); //account for offset
 }
 
 void print_lcd(char *stuff_to_display){
@@ -270,13 +281,15 @@ char idle(void){
 void coin_ctor(void){
 	print_lcd((char*)"Please Insert Coin");
 	coin = false;
+	//printf("COIN_CTOR: %d\n", coin);
 }
 
 char waitforcoin(void){
+	//printf("WAITING ... : %d\n", coin);
 	if(timer_flag(&timer3)){
-	   	// Just Indicate Status 
+		// Just Indicate Status 
 		timer_lower(&timer3);
-		 led_toggle(&led1);
+		led_toggle(&led1);
 	}
 	return coin? SETUP_BOARD : WAIT_COIN;
 }
@@ -292,6 +305,13 @@ char setup(void){
 	do_balance();
 	startgame_flag = false;
 	return WAIT_PLAYERS;
+}
+
+void wait_player_ctor(void){
+	print_lcd((char*)"Player 2:  Place ball at start and press START button when ready.");
+
+	player2ready = false;
+	ballinplace = false;
 }
 
 char wait_players(void){
@@ -318,7 +338,7 @@ void run_ctor(void){
 	remaining_time = 240;
 	timelimit = false;
 	endlimit = false;
-  pin_write(BALL_RELEASE_SERVO, calc_servo_pos(90));
+	pin_write(W_B_SERVO_PIN, calc_servo_pos(90));
 }
 
 char run(void){
@@ -340,20 +360,23 @@ char run(void){
 char end(void){
 	coin = false;
 	char s[32] = {};
+	printf("sfx:end\n");
+
 	sprintf(s, "Game Over : Player %d Wins!",endlimit?1:2);
 	print_lcd(s);
-  pin_write(BALL_RELEASE_SERVO, calc_servo_pos(0));
+
+	pin_write(W_B_SERVO_PIN, calc_servo_pos(0));
 	//return END;
-	return WAIT_COIN;
+	return IDLE;
 }
 
 State s_idle = {idle_ctor,idle,null_func};
 State s_wait_coin= {coin_ctor,waitforcoin,null_func};
 State s_setup= {null_func,setup,null_func};
-State s_wait_players= {null_func,wait_players,null_func};
+State s_wait_players= {wait_player_ctor,wait_players,null_func};
 State s_run={run_ctor,run,null_func};
 State s_end={null_func,end,null_func};
-State* states[] = {&s_wait_coin,&s_setup,&s_wait_players,&s_run,&s_end};
+State* states[] = {&s_idle, &s_wait_coin,&s_setup,&s_wait_players,&s_run,&s_end};
 
 int16_t main(void) {
 	init_clock();
@@ -363,37 +386,40 @@ int16_t main(void) {
 	init_oc();
 	init_spi();
 	init_timer();
-    timer_initDelayMicro(&timer5);
+	timer_initDelayMicro(&timer5);
 	init_lcd();
 	init_uart();
 
-	//printf("Init\n");
 	int cnt = 0;
 
-	oc_servo(&oc1, Y_SERVO_PIN, &timer1, 20e-3, 660e-6, 2340e-6, calc_servo_pos(0));
-	oc_servo(&oc2, X_SERVO_PIN, &timer1, 20e-3, 660e-6, 2300e-6, calc_servo_pos(0));
-	oc_servo(&oc3, C_SERVO_PIN, &timer1, 20e-3, 660e-6, 2340e-6, calc_servo_pos(0));
-	oc_servo(&oc4, L_SERVO_PIN, &timer1, 20e-3, 660e-6, 2340e-6, calc_servo_pos(0));
-	oc_servo(&oc5, R_SERVO_PIN, &timer1, 20e-3, 660e-6, 2340e-6, calc_servo_pos(0));
-	oc_servo(&oc6, R_SERVO_PIN, &timer1, 20e-3, 660e-6, 2340e-6, calc_servo_pos(0));
+	oc_servo(&oc1, W_Y_SERVO_PIN, &timer1, 20e-3, 660e-6, 2340e-6, calc_servo_pos(0));
+	oc_servo(&oc2, W_X_SERVO_PIN, &timer1, 20e-3, 660e-6, 2300e-6, calc_servo_pos(0));
+	oc_servo(&oc3, W_C_SERVO_PIN, &timer1, 20e-3, 660e-6, 2340e-6, calc_servo_pos(0));
+	oc_servo(&oc4, W_L_SERVO_PIN, &timer1, 20e-3, 660e-6, 2340e-6, calc_servo_pos(0));
+	oc_servo(&oc5, W_R_SERVO_PIN, &timer1, 20e-3, 660e-6, 2340e-6, calc_servo_pos(0));
+	//oc_servo(&oc6, W_B_SERVO_PIN, &timer1, 20e-3, 660e-6, 2340e-6, calc_servo_pos(0));
 
-	pin_digitalIn(COIN_PIN);
-	int_attach(&int1, COIN_PIN, INT_FALLING, &coin_inserted);
-	pin_digitalIn(END_PIN);
-	int_attach(&int2, END_PIN, INT_FALLING, &end_reached);
-	pin_digitalIn(START_PIN);
-	int_attach(&int3, START_PIN, INT_FALLING, &player_ready);
-	// pin_digitalIn(BALL_START_PIN);
-	// int_attach(&int4, BALL_START_PIN, INT_FALLING, &ball_ready);
+	pin_digitalIn(R_COIN_PIN);
+	int_attach(&int1, R_COIN_PIN, INT_FALLING, &coin_inserted);
+	pin_digitalIn(R_END_PIN);
+	int_attach(&int2, R_END_PIN, INT_FALLING, &end_reached);
+	pin_digitalIn(R_START_PIN);
+	int_attach(&int3, R_START_PIN, INT_FALLING, &player_ready);
 
+	pin_digitalOut(W_ELECTRO_PIN);
 
-	pin_digitalIn(ELECTRO_READ_PIN);
+	// pin_digitalIn(BALL_R_START_PIN);
+	// int_attach(&int4, BALL_R_START_PIN, INT_FALLING, &ball_ready);
 
-	pin_analogIn(C_READ_PIN);
+	pin_analogIn(R_ELECTRO_PIN);
+
+	pin_analogIn(R_C_SERVO_PIN);
 
 	led_on(&led1);
 	timer_setPeriod(&timer3, 0.5);
 	timer_start(&timer3);
+
+	timer_setPeriod(&timer4, 0.5);
 
 	registerUSBEvents();
 
@@ -404,40 +430,19 @@ int16_t main(void) {
 
 	bool game_on = true;
 	//state = SETUP_BOARD;
-	unsigned char state_id = WAIT_COIN;
+	unsigned char state_id = WAIT_PLAYERS;
 	states[state_id]->ctor();
 
 	while(game_on){
 		ServiceUSB();
-// 
-// 		// printf("[%d : %d]\n", state, pin_read(L_READ_PIN));
-// 		switch(state){
-// 			case WAIT_COIN:
-// 				waitforcoin();
-// 				break;
-// 			case SETUP_BOARD:
-// 				setup();
-// 				break;
-// 			case WAIT_PLAYERS:
-// 				wait_players();
-// 				break;
-// 			case RUN:
-// 				run();
-// 				break;
-// 			case END:
-// 				end();
-// 				break;
-// 			default:
-// 				break;
-
 		// State* current_state = states[state_id];
 		// unsigned char next_state_id = current_state->exec();
 		unsigned char next_state_id = states[state_id]->exec();
+		//printf("State : %d -> %d\n", state_id, next_state_id);
 		if(next_state_id != state_id){ // == transition
 			states[state_id]->dtor();
 			state_id = next_state_id;
 			states[state_id]->ctor();
 		}
-		//printf("State : %d\n", next_state_id);
 	}
 }
